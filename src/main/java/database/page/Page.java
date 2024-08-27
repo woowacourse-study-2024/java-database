fa5bfb4d24f31298f637eabc92333823d7267f6b
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-28 02:23:53 +0900

    feat: 페이지 객체와 기본 필드 정의

--- src/main/java/database/page/Page.java
+++ src/main/java/database/page/Page.java
@@ -1,19 +1,19 @@
 package database.page;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Page {
-    private final long pageNum;
-    private final byte[] data;
 
-    public Page(long pageNum, byte[] data) {
-        this.pageNum = pageNum;
-        this.data = data;
-    }
+    private static final int PAGE_SIZE = 16 * 1024;
 
-    public long getPageNum() {
-        return pageNum;
-    }
+    private final PageHeader header;
+    private final List<Record> records;
+    private int freeSpace;
 
-    public byte[] getData() {
-        return data;
+    public Page(int pageNum, PageType pageType) {
+        this.header = new PageHeader(pageNum, pageType);
+        this.records = new ArrayList<>();
+        this.freeSpace = PAGE_SIZE - header.getHeaderSize();
     }
 }
