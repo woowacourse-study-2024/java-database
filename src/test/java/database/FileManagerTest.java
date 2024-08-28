d3f1c1520f4ea58e3448e63806488e1b8f873c43
Seokmyung Ham <gkatjraud1@gmail.com>
2024-08-28 23:24:02 +0900

    style: 사용하지 않는 Import 제거

--- src/test/java/database/FileManagerTest.java
+++ src/test/java/database/FileManagerTest.java
@@ -3,7 +3,6 @@ package database;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
